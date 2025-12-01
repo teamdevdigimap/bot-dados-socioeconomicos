e9f8e48bb26dad14ebb4a9b83b151d345064265f
Matheus Augusto de Souza <mataugusto1999@gmail.com>
2025-12-01 09:45:46 -0300

    feat: adicona mapeamento de execução

--- Emprego.py
+++ Emprego.py
@@ -10,41 +10,54 @@ from tables.Emprego.table_salario_medio import table_salario_medio
 def emprego_run():
     
     try:
+        print("\nIniciando table_num_admitidos_desligados...\n")
         table_num_admitidos_desligados.run_table_num_admitidos_desligados()
+        print("\nFinalizando table_num_admitidos_desligados...\n")
     except Exception as e:
         print(f"Erro na tabela table_num_admitidos_desligados \n{e}")  
         
         
     try:
+        print("\nIniciando table_num_vinculos_setor_tamanho...\n")
         table_num_vinculos_setor_tamanho.run_table_num_vinculos_setor_tamanho()
+        print("\nFinalizando table_num_vinculos_setor_tamanho...\n")
     except Exception as e:
         print(f"Erro na tabela table_num_vinculos_setor_tamanho \n{e}")   
         
         
     try:
+        print("\nIniciando table_massa_salarial_setor_tamanho...\n")
         table_massa_salarial_setor_tamanho.run_table_massa_salarial_setor_tamanho()
+        print("\nFinalizando table_massa_salarial_setor_tamanho...\n")
     except Exception as e:
         print(f"Erro na tabela table_massa_salarial_setor_tamanho \n{e}")           
         
         
     try:
+        print("\nIniciando table_num_estabelecimento_setor_tamanho...\n")
         table_num_estabelecimento_setor_tamanho.run_table_num_estabelecimento_setor_tamanho()
+        print("\nFinalizando table_num_estabelecimento_setor_tamanho...\n")
     except Exception as e:
         print(f"Erro na tabela table_num_estabelecimento_setor_tamanho \n{e}")      
         
                  
     try:
+        print('\nIniciando table_pib_municipal_e_desagregacoes...\n')
         table_pib_municipal_e_desagregacoes.run_table_pib_municipal_e_desagregacoes()
+        print('\nFinalizando table_pib_municipal_e_desagregacoes...\n')
     except Exception as e:
         print(f"Erro na tabela table_pib_municipal_e_desagregacoes \n{e}")      
         
     try:
+        print('\nIniciando table_pib_per_capita_municipal...\n')
         table_pib_per_capita_municipal.run_table_pib_per_capita_municipal()
-        
+        print('\nFinalizando table_pib_per_capita_municipal...\n')
     except Exception as e:
         print(f"Erro na tabela table_pib_per_capita_municipal \n{e}")     
         
     try:
+        print('\nIniciando table_salario_medio...\n')
         table_salario_medio.run_table_salario_medio()
+        print('\nFinalizando table_salario_medio...\n')
     except Exception as e:
         print(f"Erro na tabela table_salario_medio \n{e}")           
--- Renda.py
+++ Renda.py
@@ -8,34 +8,44 @@ from tables.Renda.table_populacao_ocupada import table_populacao_ocupada
 def renda_run():
    
     try:
+        print("\nIniciando table_gini...\n")
         table_gini.run_table_gini()
+        print("\nFinalizando table_gini...\n")
         pass
     except Exception as e:
         print(f"Erro na tabela table_gini \n{e}")
 
     try:
+        print("\nIniciando table_cadunico...\n")
         table_cadunico.run_table_cadunico()
+        print("\nFinalizando table_cadunico...\n")
         pass
     except Exception as e:
         print(f"Erro na tabela table_cadunico \n{e}")
 
 
     try:
+        print("\nIniciando table_bpc...\n")
         table_bpc.run_table_bpc()
+        print("\nFinalizando table_bpc...\n")
         pass
     except Exception as e:
         print(f"Erro na tabela table_bpc \n{e}")
 
 
     try:
+        print("\nIniciando table_bolsa_familia...\n")
         table_bolsa_familia.run_table_bolsa_familia()
+        print("\nFinalizando table_bolsa_familia...\n")
         pass
     except Exception as e:
         print(f"Erro na tabela table_bolsa_familia \n{e}")    
 
 
     try:
+        print("\nIniciando table_populacao_ocupada...\n")
         table_populacao_ocupada.run_table_populacao_ocupada()
+        print("\nFinalizando table_populacao_ocupada...\n")
         pass
     except Exception as e:
         print(f"Erro na tabela table_populacao_ocupada \n{e}")   
